60e61f42d23349daa6ee0018adce2281969a49b5
Sreekaran <sreekaran.srinath@gmail.com>
2020-08-05 14:28:18 +0530

    Required exercises finished. Will get back to optionals later.

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    
+    error = (X * theta) - y; 
+    temp0 = theta(1) - ( alpha /m ) * sum(error.* X(:,1));
+    temp1 = theta(2) - ( alpha /m ) * sum(error.* X(:,2));
+    theta = [temp0; temp1];
 
     % ============================================================
 
